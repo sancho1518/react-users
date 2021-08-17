5b8ce58e216950e8525810eb652bb52b1c56b906
Sadiqwahab <sanchosadiq@gmail.com>
2021-08-17 13:31:06 +0000

    add userActions.js

---
+++ src/actions/userActions.js
@@ -0,0 +1,12 @@
+export const ADD_USER = "ADD_USER"
+
+export const addUser = (user) => {
+
+    user.id = Math.random().toString();
+    return{
+        type: "ADD_USER" ,
+        payload :user
+    }
+}
+
+
